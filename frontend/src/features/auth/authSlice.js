fb85f8cc5f70500f31e395e2003c8161030c0fae
Abass <abass_gbolahan@yahoo.com>
2022-12-17 23:20:06 +0000

    register user

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,8 +1,11 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import authService from './authService'
 
+//Get user from Local Storage
+const user = JSON.parse(localStorage.getItem('user'))
 
 const initialState = {
-    user:null,
+    user:user ? user : null,
     isError: false,   //if we get an error from the server
     isSuccess: false,
     isLoading: false,
@@ -12,7 +15,15 @@ const initialState = {
 
 //Register new user
 export const register = createAsyncThunk('auth/register', async(user, thunkAPI) => {
-    console.log(user)
+    try {
+       return await authService.register(user)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message)
+        || error.message || error.toString()
+
+        return thunkAPI.rejectWithValue(message)
+
+    }
 })
 
 //Login user
@@ -25,10 +36,31 @@ export const login = createAsyncThunk('auth/login', async(user, thunkAPI) => {
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
-    reducers: {},
+    reducers: {
+        reset: (state) => {
+            state.isError = false
+            state.isLoading = false
+            state.isSuccess = false
+            state.message = ''
+        }
+    },
     extraReducers: (builder) => {
+        builder.addCase(register.pending, (state)=> {
+            // when pending
+            state.isLoading = true
+        }).addCase(register.fulfilled, (state, action) =>{
+            state.isLoading = false
+            state.isSuccess = true
+            state.user = action.payload
 
+        }).addCase(register.rejected, (state, action) =>{
+            state.isLoading = false
+            state.user = null
+            state.isError = true
+            state.message = action.payload
+        })
     }
 })
 
+export const {reset} = authSlice.actions
 export default authSlice.reducer
