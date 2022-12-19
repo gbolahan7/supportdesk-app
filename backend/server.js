8251123a2da24c8062903b45057869bef0650c1c
Abass <abass_gbolahan@yahoo.com>
2022-12-19 15:35:10 +0000

    app deploy

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,14 @@ if(process.env.NODE_ENV === 'production') {
     // set build folder as static
     app.use(express.static(path.join(__dirname, '../frontend/build')))
     //loading index.html in the build folder
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html' ))
-} else {
-    app.get('/', (req, res) => {
-        res.status(200).json({message: 'Welcome to support desk API'})
-    })
-} 
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+      })
+    } else {
+      app.get('/', (_, res) => {
+        res.status(200).json({ message: 'Welcome to the Support Desk API' })
+      })
+    }
 
 
 app.use(errorHandler)
